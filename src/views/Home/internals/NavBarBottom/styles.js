152786e1a89c08ac554b6aeff94726cb254f2622
Rodrigo Gomes Araújo <rodrigoaraujo990@gmail.com>
2020-06-09 22:52:59 -0300

    finish menu-mobile

--- src/views/Home/internals/NavBarBottom/styles.js
+++ src/views/Home/internals/NavBarBottom/styles.js
@@ -1,14 +1,11 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.ul`
   width: 100%;
-  padding: 20px 20px;
-  @media screen and (max-width: 1000px) {
-    display: none;
-  }
+  padding: 10px 20px;
 
   .content {
-    max-width: 1100px;
+    max-width: 1138px;
     width: 100%;
     margin: 0 auto;
     list-style: none;
@@ -24,6 +21,54 @@ export const Container = styled.ul`
         font-size: 0.9em;
         text-decoration: none;
         color: black;
+      }
+    }
+    @media screen and (max-width: 920px) {
+      display: none;
+    }
+  }
+
+  .menu-mobile {
+    position: absolute;
+    top: 50px;
+    right: 50px;
+    display: none;
+    justify-content: flex-end;
+    width: 100%;
+    max-width: 1138px;
+    transition: all 0.3s;
+    svg {
+      color: var(--primary);
+      cursor: pointer;
+    }
+    @media screen and (max-width: 620px) {
+      right: 20px;
+      top: 85px;
+    }
+    @media screen and (max-width: 920px) {
+      display: flex;
+    }
+    .list-menu-mobile {
+      padding: 30px;
+      z-index: 9999;
+      background-color: var(--primary);
+      position: absolute;
+      top: 30px;
+      list-style: none;
+      display: none;
+      ${(props) =>
+        props.isVisible &&
+        css`
+          display: flex;
+        `}
+      flex-direction: column;
+      a {
+        text-decoration: none;
+        color: white;
+        font-size: 1.3em;
+        &:hover {
+          text-decoration: underline;
+        }
       }
     }
   }
